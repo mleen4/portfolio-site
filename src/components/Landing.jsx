4c6e74fda8d5c00b381c4106db07288e6b7d6427
Michael <Michael.leenj1999@gmail.com>
2022-11-19 21:47:48 +1100

    Added Landing page

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -4,17 +4,20 @@ import React from "react";
 function Landing() {
     return (
         <div className="landing-container">
-            <div className="landing-title">
-
+            <div className="landing-container-left">
+                <div className="landing-splash">
+                    {/* Insert art here */}
+                </div>
             </div>
+            <div className="landing-container-right">
+                <div className="landing-title">
+                    <h1>&lt; Michael <br/> Leen /&gt;</h1>
+                </div>
 
-            <div className="landing info">
-
+                <div className="landing-info">
+                    <h3>Deakin University IT Pre-graduate</h3>
+                </div>
             </div>
-            <div className="landing-splash">
-                {/* Insert art here */}
-            </div>
-        <h1>Header</h1>
         </div>
     )
 }
