d4bfb7e82e38235b3a74652793acdc96ca33be24
Michael <Michael.leenj1999@gmail.com>
2022-11-26 17:33:56 +1100

    Testimonial base created

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,10 +1,31 @@
-import React from "react";
+import {React, useState} from "react";
+import testimonialObjects from "../testimonialObjects";
+
 
 
 function Testimonials() {
+    const [testimonial, handleTestimonialChange] = useState(0)
+
+    const handleCarosel = () => {
+
+    }
+
+
+
+
     return (
-        <div className="testimonials">
-        <h1>Testimonials</h1>
+        <div className="testimonials-container">
+            <div className="testimonials-carousel">
+                <div className="testimonials-carousel-right-arrow"></div>
+                <div className="testimonials-carousel-person-container">
+                    <div className="testimonial-person-image"></div>
+                    <div className="testimonial-person-name"></div>
+                    <div className="testimonial-person-role"></div>
+                    <div className="testimonial-person-quote"></div>
+                    
+                </div>
+                <div className="testimonials-carousel-left-arrow"></div>
+            </div>
         </div>
     )
 }
---
+++ src/testimonialObjects.js
@@ -0,0 +1,5 @@
+var testimonialObjects = [
+
+]
+
+export default testimonialObjects
